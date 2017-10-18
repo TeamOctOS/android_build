f137ca2fdd71d324044d59db3bdd08d70d84a3dd
cjkacz <cjkacz@gmail.com>
2017-10-18 06:55:04 -0400

    Fix fastpath code
    Change-Id: If56e1b79a4259152128755c84c6618ecd8447211

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,7 +185,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
  ifneq ($(OCTOS_BUILD),)
-    all_product_configs := device/*/$(OCTOS_BUILD)/octos.mk
+    all_product_configs := $(shell ls device/*/$(OCTOS_BUILD)/octos.mk
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
