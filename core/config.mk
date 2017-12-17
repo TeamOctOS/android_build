1912c1b6ac867f18024adb565ae23bf06e85ddc8
Daniel Sikorski <dadi.sikorski@gmail.com>
2017-12-17 11:45:19 -0500

    build: remove octos/sepolicy
    * we don't need it since it contains only lineage stuff
    
    Change-Id: I7a5417152115176c6b55f55946c021ae0a388486

--- core/config.mk
+++ core/config.mk
@@ -898,10 +898,6 @@ include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
 ifneq ($(OCTOS_BUILD),)
-## We need to be sure the global selinux policies are included
-## last, to avoid accidental resetting by device configs
-$(eval include device/octos/sepolicy/common/sepolicy.mk)
-
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
 
